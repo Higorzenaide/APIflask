8a8931797ee458a46513abfdee9e08010f8c1ffa
Higorzenaide <higorz.almeida@gmail.com>
2024-01-18 23:43:15 -0300

    Att

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 import os
 import json
@@ -11,10 +11,10 @@ API_KEY = os.getenv("API_KEY")
 DATABASE_URL = os.getenv("OUTRA_VARIAVEL")
 
 class BancoDeDados:
-  def __init__(self):
-      self.client = create_client(DATABASE_URL, API_KEY)
+    def __init__(self):
+        self.client = create_client(DATABASE_URL, API_KEY)
       
-  def visualizarDados(self,id):
+    def visualizarDados(self, id):
         response, data = self.client.table('feedbacks').select('motivo_macro', 'motivo', 'Nome_colaborador', 'date').eq('id_gestor', id).execute()
         response_string = response[1]
         resposta = json.loads(json.dumps(response_string))
@@ -22,10 +22,11 @@ class BancoDeDados:
 
 app = Flask(__name__)
 
-@app.route('/consultar/<id>',methods=['GET','POST'])
-def hello():
+@app.route('/consultar/<int:id>', methods=['GET', 'POST'])
+def hello(id):
     instanciar_teste = BancoDeDados()
-    return instanciar_teste.visualizarDados(id)
+    retorno = instanciar_teste.visualizarDados(id)
+    return jsonify(retorno)
 
 if __name__ == '__main__':
     app.run(debug=True)
