760e2b7275d497df798842c22bd7f30e4df43db7
Higorzenaide <higorz.almeida@gmail.com>
2024-01-19 10:14:55 -0300

    Att

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
 from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 import os
-import json
-from supabase import create_client
-from dataBase import BancoDeDados
+from dataBase import BancoDeDados  # Certifique-se de que esta importação está correta
 
 # Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
 
-API_KEY = os.getenv("API_KEY")
-DATABASE_URL = os.getenv("OUTRA_VARIAVEL")
-
 app = Flask(__name__)
 
-@app.route('/consultar/<senha>,<email>', methods=['GET'])
-def hello(senha,email):
+@app.route('/consultar/<senha>/<email>', methods=['GET'])
+def consultar(senha, email):
+    # Suponho que você tenha uma função VerificaLogin no seu BancoDeDados
+    # Certifique-se de que a classe BancoDeDados e a função VerificaLogin estejam corretamente definidas
     instanciar_teste = BancoDeDados()
-    retorno = instanciar_teste.VerificaLogin(senha,email)
+    retorno = instanciar_teste.VerificaLogin(senha, email)
     return jsonify(retorno)
 
 if __name__ == '__main__':
