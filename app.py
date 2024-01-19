b484f3ea20a2c50217ea56b9e815fc4e757c8426
Higorzenaide <higorz.almeida@gmail.com>
2024-01-19 09:41:55 -0300

    Att

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 import json
 from supabase import create_client
+from moldes.dataBase import create_client
 
 # Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
@@ -10,21 +11,11 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 DATABASE_URL = os.getenv("OUTRA_VARIAVEL")
 
-class BancoDeDados:
-    def __init__(self):
-        self.client = create_client(DATABASE_URL, API_KEY)
-      
-    def visualizarDados(self, id):
-        response, data = self.client.table('feedbacks').select('motivo_macro', 'motivo', 'Nome_colaborador', 'date').eq('id_gestor', id).execute()
-        response_string = response[1]
-        resposta = json.loads(json.dumps(response_string))
-        return resposta
-
 app = Flask(__name__)
 
 @app.route('/consultar/<id>', methods=['GET', 'POST'])
 def hello(id):
-    instanciar_teste = BancoDeDados()
+    instanciar_teste = create_client()
     retorno = instanciar_teste.visualizarDados(id)
     return jsonify(retorno)
 
