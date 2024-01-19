1e02bbb51470cf3a568585d022cf6d81ce0a16bc
Higorzenaide <higorz.almeida@gmail.com>
2024-01-18 22:50:45 -0300

    att

--- api/app.py
+++ api/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from dotenv import load_dotenv
+import os
 
+api_key = os.getenv("API_KEY")
+outra_variavel = os.getenv("OUTRA_VARIAVEL")
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'testando se está merda ta funfando'
+    return 'api_key'
 
 if __name__ == '__main__':
     app.run(debug=True)
