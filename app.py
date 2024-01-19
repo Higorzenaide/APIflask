c3503d63e146d5413a70752c9a3cc4aeae908472
Higorzenaide <higorz.almeida@gmail.com>
2024-01-19 10:21:40 -0300

    Att

--- app.py
+++ app.py
@@ -1,10 +1,5 @@
-from flask import Flask, request, jsonify
-from dotenv import load_dotenv
-import os
+from flask import Flask
 from dataBase import BancoDeDados  # Certifique-se de que esta importação está correta
-
-# Carrega as variáveis de ambiente do arquivo .env
-load_dotenv()
 
 app = Flask(__name__)
 
@@ -14,7 +9,7 @@ def consultar(senha, email):
     # Certifique-se de que a classe BancoDeDados e a função VerificaLogin estejam corretamente definidas
     instanciar_teste = BancoDeDados()
     retorno = instanciar_teste.VerificaLogin(senha, email)
-    return jsonify(retorno)
+    return retorno
 
 if __name__ == '__main__':
     app.run(debug=True)
--- dataBase.py
+++ dataBase.py
@@ -4,7 +4,6 @@ import os
 import json
 from supabase import create_client
 
-
 # Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
 
@@ -15,9 +14,9 @@ class BancoDeDados:
     def __init__(self):
         self.client = create_client(DATABASE_URL, API_KEY)
       
-    def VerificaLogin(self, senha,email):
+    def VerificaLogin(self,senha,email):
         try:
-            response, data = self.client.table('users').select('id','Gestor','email','verificado','supervisao','treinamentos').eq('senha',senha,'email',email).execute()
+            response, data = self.client.table('users').select('id','Gestor','email','verificado','supervisao','treinamentos').eq('senha',senha).eq('email',email).execute()
             response_string = response[1]
             resposta = json.loads(json.dumps(response_string))
             return resposta
