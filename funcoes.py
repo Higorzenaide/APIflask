e27a81ce6e750493098e5cd9e022c269518c70ff
Higorzenaide <higorz.almeida@gmail.com>
2024-01-30 11:51:39 -0300

    LogV03

--- funcoes.py
+++ funcoes.py
@@ -30,10 +30,10 @@ def verificarConflitos(AgendamentosJaexistentes,dados_de_novo_agendamento):
     print(f'lista_de_hora_fim_ja_agendada {lista_de_hora_fim_ja_agendada}')
     
     for hora_inicio_ja_agendada, hora_fim_ja_agendada in zip(lista_de_hora_inicio_ja_agendada,lista_de_hora_fim_ja_agendada):
-        hora_inicio_ja_agendada = pd.to_datetime(hora_inicio_ja_agendada)
-        hora_fim_ja_agendada = pd.to_datetime(hora_fim_ja_agendada)
-        nova_hora_inicio = pd.to_datetime(nova_hora_inicio)
-        nova_hora_fim = pd.to_datetime(nova_hora_fim)
+        # hora_inicio_ja_agendada = pd.to_datetime(hora_inicio_ja_agendada)
+        # hora_fim_ja_agendada = pd.to_datetime(hora_fim_ja_agendada)
+        # nova_hora_inicio = pd.to_datetime(nova_hora_inicio)
+        # nova_hora_fim = pd.to_datetime(nova_hora_fim)
 
         if nova_hora_inicio >= hora_inicio_ja_agendada and nova_hora_inicio <= hora_fim_ja_agendada:
             print(F'-----------ENTROU NO PRIMEIRO-----------------')
